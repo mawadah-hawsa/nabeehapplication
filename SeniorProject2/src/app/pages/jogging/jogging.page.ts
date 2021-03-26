413272f04063cb746d0a32930c6e8ed613980ddc
jumanaib <jumanahbm@gmail.com>
2021-03-26 20:27:12 +0300

    Create jogging.page.ts

---
+++ SeniorProject2/src/app/pages/jogging/jogging.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jogging',
+  templateUrl: './jogging.page.html',
+  styleUrls: ['./jogging.page.scss'],
+})
+export class JoggingPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
