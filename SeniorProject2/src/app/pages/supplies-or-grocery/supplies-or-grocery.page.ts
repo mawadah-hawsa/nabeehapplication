37f944feae5205cdb89536c59c3758476026f12f
jumanaib <jumanahbm@gmail.com>
2021-03-26 20:31:03 +0300

    Create supplies-or-grocery.page.ts

---
+++ SeniorProject2/src/app/pages/supplies-or-grocery/supplies-or-grocery.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplies-or-grocery',
+  templateUrl: './supplies-or-grocery.page.html',
+  styleUrls: ['./supplies-or-grocery.page.scss'],
+})
+export class SuppliesOrGroceryPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
