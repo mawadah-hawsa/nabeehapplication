744923d656c20627cad51b9c27225f8a98ec2d87
jumanaib <jumanahbm@gmail.com>
2021-03-26 20:28:10 +0300

    Create other-emergency.page.ts

---
+++ SeniorProject2/src/app/pages/other-emergency/other-emergency.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-emergency',
+  templateUrl: './other-emergency.page.html',
+  styleUrls: ['./other-emergency.page.scss'],
+})
+export class OtherEmergencyPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
